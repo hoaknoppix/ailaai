9bc3e22ba675efc9947ab7e05a02e366ff4242b3
Hoà Trần <hoa.tran@giffgaff.co.uk>
2023-11-02 22:49:51 +0700

    Draft skeleton design for group view

---
+++ iosApp/iosApp/LocalView.swift
@@ -0,0 +1,21 @@
+//
+//  LocalView.swift
+//  iosApp
+//
+//  Created by Hoa Tran on 11/2/23.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct LocalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LocalView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocalView()
+    }
+}
